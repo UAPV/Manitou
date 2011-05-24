48e8b5aaf70b14e21ca341819499433ee5430005
ArnaudD <arnaud.didry@gmail.com>
2011-05-24 23:33:38 +0000

    Affichage de la configuration générée

---
+++ apps/backend/modules/dhcpd/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * dhcpd actions.
+ *
+ * @package    DRBL Admin 2
+ * @subpackage dhcpd
+ * @author     Arnaud Didry <arnaud.didry@univ-avignon.fr>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class dhcpdActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->dhcpdConf = file_get_contents(sfConfig::get ('sf_data_dir').'/dhcpd/dhcpd.conf');
+  }
+}
