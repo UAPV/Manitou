c7f777815a1596582d21350ca6f81bfc66038b8d
Arnaud Didry <arnaud@didry.info>
2011-07-05 18:12:17 +0200

    Traduction des labels + aide

--- lib/form/HostForm.class.php
+++ lib/form/HostForm.class.php
@@ -13,6 +13,30 @@ class HostForm extends BaseHostForm
   {
     unset ($this['created_at']);
     unset ($this['updated_at']);
+    
+    $this->widgetSchema->setLabels (array (
+      'profile_id'           => 'Profil',
+      'room_id'              => 'Salle',
+      'number'               => 'Suffixe',
+      'ip_address'           => 'Adresse IP',
+      'mac_address'          => 'Adresse MAC',
+      'comment'              => 'Commentaire',
+      'custom_conf'          => 'Conf DHCP',
+      'cloned_from_image_id' => 'Image système',
+      'subnet_id'            => 'Subnet',
+      'pxe_file_id'          => 'Fichier PXE',
+    ));
 
+    $this->widgetSchema->setHelps (array (
+      'profile_id'           => 'Profil de l\'utilisateur',
+      'room_id'              => 'Salle où se situe la machine',
+      'number'               => 'Numéro utilisé comme suffixe dans le hostname',
+      'ip_address'           => 'Adresse IP',
+      'mac_address'          => 'Adresse MAC (format 00:11:22:33:44:55)',
+      'comment'              => 'Commentaire (ex: numéro de prise, etc)',
+      'custom_conf'          => 'Conf DHCP personnalisée',
+      'cloned_from_image_id' => 'Dernière image système appliquée',
+      'pxe_file_id'          => 'Fichier PXE (par défaut celui configuré pour le subnet sera utilisé)',
+    ));
   }
 }
