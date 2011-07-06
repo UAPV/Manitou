3ebb9301923a24f161e4e5493256a11c57667b4e
Arnaud Didry <arnaud@didry.info>
2011-07-06 09:38:39 +0200

    Ajout de la colonne gateway dans le subnet

---
+++ lib/model/migration/PropelMigration_1309937176.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1309937176.
+ * Generated on 2011-07-06 09:26:16 by lezard
+ */
+class PropelMigration_1309937176
+{
+
+	public function preUp($manager)
+	{
+		// add the pre-migration code here
+	}
+
+	public function postUp($manager)
+	{
+		// add the post-migration code here
+	}
+
+	public function preDown($manager)
+	{
+		// add the pre-migration code here
+	}
+
+	public function postDown($manager)
+	{
+		// add the post-migration code here
+	}
+
+	/**
+	 * Get the SQL statements for the Up migration
+	 *
+	 * @return array list of the SQL strings to execute for the Up migration
+	 *               the keys being the datasources
+	 */
+	public function getUpSQL()
+	{
+		return array (
+  'propel' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `subnet` ADD
+(
+	`gateway` VARCHAR(15) NOT NULL
+);
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+	}
+
+	/**
+	 * Get the SQL statements for the Down migration
+	 *
+	 * @return array list of the SQL strings to execute for the Down migration
+	 *               the keys being the datasources
+	 */
+	public function getDownSQL()
+	{
+		return array (
+  'propel' => '
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+ALTER TABLE `subnet` DROP `gateway`;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
+',
+);
+	}
+
+}
