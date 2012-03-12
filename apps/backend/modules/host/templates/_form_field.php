2951fec84da0aa6389605135eccb342c27aeb0be
fannyny <marcelf@marcelf-OptiPlex-760.(none)>
2012-03-12 12:10:55 +0100

    ajout du bouton pour vérifier la disponibilité de l'hostname dans le dns

---
+++ apps/backend/modules/host/templates/_form_field.php
@@ -0,0 +1,24 @@
+<?php if ($field->isPartial()): ?>
+  <?php include_partial('host/'.$name, array('form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?>
+<?php elseif ($field->isComponent()): ?>
+  <?php include_component('host', $name, array('form' => $form, 'attributes' => $attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes)) ?>
+<?php else: ?>
+  <div class="<?php echo $class ?><?php $form[$name]->hasError() and print ' errors' ?>">
+    <?php echo $form[$name]->renderError() ?>
+    <div>
+      <?php echo $form[$name]->renderLabel($label) ?>
+
+
+      <?php if ($help): ?>
+        <div class="help"><?php echo __($help, array(), 'messages') ?></div>
+      <?php elseif ($help = $form[$name]->renderHelp()): ?>
+        <div class="help"><?php echo $help ?></div>
+      <?php endif; ?>
+    </div>
+
+      <div class="content">
+          <?php echo $form[$name]->render($attributes instanceof sfOutputEscaper ? $attributes->getRawValue() : $attributes) ?>
+          <?php if($name == 'number') echo '<br/><button id="testDns">Vérifier la disponibilité du nom.</button>'?>
+      </div>
+  </div>
+<?php endif; ?>
