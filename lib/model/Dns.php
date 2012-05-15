ed15fb7a55ed81fd0e5598448ec59c9a643d7569
fannyny <marcelf@marcelf-OptiPlex-760.(none)>
2012-05-15 17:52:51 +0200

    debug

--- lib/model/Dns.php
+++ lib/model/Dns.php
@@ -282,9 +282,9 @@ EOF
            //on regarde si la ligne en cours de lecture est un nouvel host
            $regex = '/^[A-Za-z0-9].*\s+IN\s+A/';
            $regexCom = '/^;+\s/';
-           if(preg_match($regex,$content[$i]) === 1)
+             echo $content[$i]."<br/>";
+             if(preg_match($regex,$content[$i]) === 1)
            {
-               echo $content[$i]."<br/>";
               //on récupère l'adresse ip pour le mettre en clé dans le tableau final
               $hostname = preg_replace('/\s+IN\s+A\s+.*/','',$content[$i]);
               $hostname = str_replace(' ','',$hostname);
