353b95ed7ef0b2ae1a5a02733b5013e5386080e4
fannyny <fanny.marcel@univ-avignon.fr>
2012-09-03 15:35:02 +0200

    affichage pour debug dans l'algo

--- lib/model/Dns.php
+++ lib/model/Dns.php
@@ -77,10 +77,8 @@ class Dns
 
               if(preg_match($regex,$content[$i]) === 1)
               {
-								  echo "resultat intial : ".$content[$i];
                   //on récupère le numéro pour le mettre en clé dans le tableau final
                   $content[$i] = preg_replace("/(\s)+/",' ',$content[$i]);
-								  echo "resultat final : ".$content[$i];
                   $tmp = explode(' ',$content[$i]);
                   $keyArray = str_replace(';','',$tmp[0]);
                   $arrayDns["$keyArray"] = array($comment,$content[$i]);
