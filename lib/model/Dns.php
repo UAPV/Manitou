fded1362ba39ecb71fdb6e78b20660c3c42649e8
fannyny <fanny.marcel@univ-avignon.fr>
2013-02-19 10:24:57 +0100

    test avec des echo

--- lib/model/Dns.php
+++ lib/model/Dns.php
@@ -80,7 +80,8 @@ class Dns
                   //on récupère le numéro pour le mettre en clé dans le tableau final
                   //$content[$i] = preg_replace("/(\s)+/",' ',$content[$i]);
                   $tmp = explode(' ',$content[$i]);
-                  $keyArray = str_replace(';','',$tmp[0]);
+								echo '<pre>';var_dump($tmp);echo '</pre>';
+								$keyArray = str_replace(';','',$tmp[0]);
                   $arrayDns["$keyArray"] = array($comment,$content[$i]);
                   unset($comment);
                   $comment = array();
