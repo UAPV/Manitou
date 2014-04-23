b4545db77997fdeb6f3326b10092e1ccfd022bd7
fannyny <fanny26f>
2014-04-23 16:38:14 +0200

    test pour mettre le commentaire manitou à la suite de l'ip et non au-dessus dans les fichiers de conf du dns

--- lib/model/Dns.php
+++ lib/model/Dns.php
@@ -417,7 +417,7 @@ EOF
              }
 
              $key = str_pad($entry['hostname'], 24);
-             $com = array("; UPDATED BY MANITOU --> DON'T TOUCH ;)");
+             $com = "; UPDATED BY MANITOU --> DON'T TOUCH ;)";
              $key = str_replace(' ','',$key);
             // $newContent = str_pad ($entry['hostname'], 24).'    IN    A    '.$entry['ip']."\n";
 					 $newContent = $entry['hostname']."\t\t".'IN'."\t".'A'."\t".$entry['ip']."  $com\n";
