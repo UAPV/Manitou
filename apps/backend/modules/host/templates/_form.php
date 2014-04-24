8b60e5c47668ee17245849341713d1295c19646d
fannyny <fanny26f>
2014-04-24 10:21:16 +0200

    ajout d'un champ si pas de profil

--- apps/backend/modules/host/templates/_form.php
+++ apps/backend/modules/host/templates/_form.php
@@ -64,10 +64,10 @@
     })
 
     //la case "pas de profil" à été cochée
-    $('#noProfile').click(function(){
+    /*$('#noProfile').click(function(){
         $('#host_profile_id').append('<option value="no" selected="selected"></option>');
         return false;
-    })
+    })*/
   });
 
   function checkDns(profile, room, suffixe, test)
