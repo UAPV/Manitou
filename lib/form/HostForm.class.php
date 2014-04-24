e2d7aad0d4be3c42b79244ddcd3ce941ba067dff
fannyny <fanny26f>
2014-04-24 11:23:06 +0200

    ajout d'un champ si pas de profil

--- lib/form/HostForm.class.php
+++ lib/form/HostForm.class.php
@@ -63,6 +63,8 @@ class HostForm extends BaseHostForm
     $this->widgetSchema['cloned_from_image_id'] = new sfWidgetFormPropelChoice (array('model' => 'Image', 'order_by' => array('Filename','asc')));
     $this->widgetSchema['cloned_from_image_id']->setLabel('Image système');
 
+    $this->validatorSchema['profile_id'] = new sfWidgetFormPropelChoice(array('model' => 'Profile', 'order_by' => array('Name','asc'), 'required' => false));
+
 	$this->widgetSchema['commentSvn'] = new sfWidgetFormInputText(array('label' => 'Commentaire SVN'));
     $this->setValidator ('ip_address', new sfValidatorIpAddress(array('required' => true)));
     $this->setValidator ('mac_address', new sfValidatorMacAddress(array('required' => true)));
