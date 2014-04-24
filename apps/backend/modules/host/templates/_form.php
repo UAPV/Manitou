b66d788dd5f18408249920dd8748bde33eee3ea3
fannyny <fanny26f>
2014-04-24 11:54:30 +0200

    ajout d'un champ si pas de profil

--- apps/backend/modules/host/templates/_form.php
+++ apps/backend/modules/host/templates/_form.php
@@ -67,6 +67,7 @@
     $('#noProfile').click(function(){
         if( $(this).is(':checked') )
         {
+            $(this).attr('checked', true);
             $('#host_profile_id').append('<option value="no" selected="selected"></option>');
             return false;
         }
