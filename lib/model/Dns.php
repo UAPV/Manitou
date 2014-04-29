6b6c92a307d69094e713bc362ff24d8373586499
fannyny <fanny26f>
2014-04-29 14:30:54 +0200

    suppression d'un echo

--- lib/model/Dns.php
+++ lib/model/Dns.php
@@ -441,7 +441,6 @@ EOF
                {
                    if(!$separation)
                    {
-                       echo "nouvelle ligne : ".$nvLigne."<br/>";
                        if(preg_match('/;\s+UPDATED\s+BY\s+MANITOU\s+/', $nvLigne) === 1)
                        {
                          $separation = true;
