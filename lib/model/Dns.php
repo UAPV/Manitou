cf90243e0d458036e6687d8edb02993e6bc70d4c
fannyny <fanny26f>
2014-05-15 09:49:07 +0200

    suppression lignes vides conf dhcp

--- lib/model/Dns.php
+++ lib/model/Dns.php
@@ -83,9 +83,7 @@ class Dns
               $regex = '/\s+IN\s+PTR\s/i';
               $regexCom = '/^;+\s/i';
 
-              if(preg_match('/;\s+UPDATED\s+BY\s+MANITOU\s+/i', $content[$i]) === 1)
-                  continue;
-              elseif(preg_match($regex,$content[$i]) === 1)
+              if((preg_match($regex,$content[$i]) === 1) AND (preg_match('/;\s+UPDATED\s+BY\s+MANITOU\s+/i', $content[$i]) !== 1))
               {
                 //on récupère le numéro pour le mettre en clé dans le tableau final
   			    $tmp = preg_split("/[\s]+/", $content[$i]);
